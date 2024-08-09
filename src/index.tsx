d4a4a6992e85e2ff11e85334e0163cca24c8d7ee
Royi Pressburger <rpressburger@microsoft.com>
2024-08-09 21:15:24 +0300

    better RTL support

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { createTheme } from '@mui/material/styles';
-
-createTheme({
-    direction: 'rtl',
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
