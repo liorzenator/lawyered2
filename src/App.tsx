6774f86f3d26e0891235ef735b293c4fd4800ac5
liorzenator <leorz@me.com>
2024-08-15 10:10:01 +0300

    adding storage blueprint

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {
   ShowGuesser,
 } from "react-admin";
 import { Layout } from "./Layout";
-import { dataProvider } from "./dataProvider";
+// import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
 import { defaultTheme } from 'react-admin';
 import createCache from '@emotion/cache';
@@ -15,6 +15,11 @@ import { CacheProvider } from '@emotion/react';
 import { ClientCreate } from "./components/clientCreate";
 import { EditClient } from "./components/clientEdit";
 import { ClientList } from "./components/clientList";
+import localStorageDataProvider from 'ra-data-local-storage';
+import simpleRestDataProvider from 'ra-data-simple-rest';
+const ServicesDataProvider = simpleRestDataProvider('http://your.server.com/api/services');
+
+const dataProvider = localStorageDataProvider();
 
 const theme = {
   ...defaultTheme,
@@ -31,7 +36,7 @@ export const App = () => (
   <CacheProvider value={cacheRtl}>
     <Admin
       layout={Layout}
-      dataProvider={dataProvider}
+      dataProvider={ServicesDataProvider}
       authProvider={authProvider}
       theme={theme}
     >
